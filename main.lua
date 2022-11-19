709764577b85b353b465a3fd2f11ab16d3e71810
jvsm123 <jvsm123@outlook.com>
2022-11-19 12:08:18 -0300

    initilization on sprites and main lógic moviment

--- main.lua
+++ main.lua
@@ -1,8 +1,34 @@
 function love.load()
+	Sprites = {}
+
+	Sprites.background = love.graphics.newImage('sprites/background.png');
+	Sprites.zombie = love.graphics.newImage('sprites/zombie.png');
+	Sprites.player = love.graphics.newImage('sprites/player.png');
+	Sprites.bullet = love.graphics.newImage('sprites/bullet.png');
+
+	Player = {};
+	Player.x = love.graphics.getWidth() / 2;
+	Player.y = love.graphics.getHeight() / 2;
+	Player.speed = 2
 end
 
-function love.update()
+function love.update(dt)
+	if love.keyboard.isDown("d") then
+		Player.x = Player.x + 1 + Player.speed;
+	end
+	if love.keyboard.isDown("a") then
+		Player.x = Player.x - 1 - Player.speed;
+	end
+	if love.keyboard.isDown("w") then
+		Player.y = Player.y - 1 - Player.speed;
+	end
+	if love.keyboard.isDown("s") then
+		Player.y = Player.y + 1 + Player.speed;
+	end
 end
 
 function love.draw()
+	love.graphics.draw(Sprites.background, 0, 0)
+
+	love.graphics.draw(Sprites.player, Player.x, Player.y)
 end
